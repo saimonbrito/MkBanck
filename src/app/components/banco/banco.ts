8b44d7262bbb793cad7a138b3d24a29ebfd566c4
saimonbrito <britomauricio89@gmail.com>
2025-11-03 13:31:23 -0300

    configurando pipes date, saldo, mundando i18n sourceLocale  para pt

--- src/app/components/banco/banco.ts
+++ src/app/components/banco/banco.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-import { Main } from "../main/main";
+import { Main } from "./main/main";
+import { DatePipe, TitleCasePipe } from '@angular/common';
+import { Saldo } from "./saldo/saldo";
 
 @Component({
   selector: 'app-banco',
-  imports: [Main],
+  imports: [Main, DatePipe, Saldo,TitleCasePipe],
   templateUrl: './banco.html',
   styleUrl: './banco.css',
 })
 export class Banco {
 
+  dataAtual = new Date();
 }
---
+++ src/app/components/banco/saldo/saldo.ts
@@ -0,0 +1,12 @@
+import { CurrencyPipe } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-saldo',
+  imports: [CurrencyPipe],
+  templateUrl: './saldo.html',
+  styleUrl: './saldo.css',
+})
+export class Saldo {
+
+}
