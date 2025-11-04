81fdad4f210e900e739be0f27e9763e4e0710195
saimonbrito <britomauricio89@gmail.com>
2025-11-04 20:53:39 -0300

    refaturando formulario

--- src/app/components/banco/main/main.ts
+++ src/app/components/banco/main/main.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './main.html',
   styleUrl: './main.css',
 })
 export class Main {
-
+  tipotransacao= "";
+  valorTransacao = "";
+   aoSubmeter() {
+    console.log(this.tipotransacao);
+    console.log(this.valorTransacao);
+    this.tipotransacao = "";
+    this.valorTransacao = "";
+  }
 }
